75c4ea3e7961da977dea174c7f8400c62a5da276
Yin-Sh31 <2254263620@qq.com>
2025-04-02 00:33:29 +0800

    first commit

---
+++ Core/Inc/control.h
@@ -0,0 +1,10 @@
+#define aleft = 0.1;  // 直角左转
+#define left = 0;     // 左转
+#define mleft = 0;    // 小左转
+#define mringt = 0.1; // 小右转
+#define ringt = 0;    // 右转
+#define aright = 0;   // 直角右转
+
+#define p = 0.1;
+#define i = 0;
+#define d = 0;
---
+++ Core/Inc/main.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define left_Pin GPIO_PIN_3
+#define left_GPIO_Port GPIOA
+#define mleft_Pin GPIO_PIN_4
+#define mleft_GPIO_Port GPIOA
+#define mright_Pin GPIO_PIN_5
+#define mright_GPIO_Port GPIOA
+#define right_Pin GPIO_PIN_6
+#define right_GPIO_Port GPIOA
+#define Reset_Pin GPIO_PIN_12
+#define Reset_GPIO_Port GPIOB
+#define Reverse_Pin GPIO_PIN_13
+#define Reverse_GPIO_Port GPIOB
+#define Forward_Pin GPIO_PIN_14
+#define Forward_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
